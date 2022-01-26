314c3c5742069af90ce4427505f56fa03a58d03d
Waleed <waleedthedev@gmail.com>
2022-01-26 19:49:18 +0500

    Clean up and setup according to style guides

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,15 @@
 <script>
-  import "../app.css";
+	import '../app.css';
 </script>
 
 <svelte:head>
-  <title>REST Countries</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap"
+		rel="stylesheet"
+	/>
+	<title>REST Countries</title>
 </svelte:head>
 
 <slot />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,20 @@
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				cstm: {
+					'dark-blue-dark-mode-elements': 'hsl(209, 23%, 22%)',
+					'very-dark-blue-dark-mode-background': 'hsl(207, 26%, 17%)',
+					'very-dark-blue-light-mode-text': 'hsl(200, 15%, 8%)',
+					'dark-gray-light-mode-input': 'hsl(0, 0%, 52%)',
+					'very-light-gray-light-mode-background': 'hsl(0, 0%, 98%)'
+				}
+			},
+			fontFamily: {
+				'nunito-sans': ['Nunito Sans', 'sans-serif']
+			}
+		}
+	},
+	plugins: []
 };
